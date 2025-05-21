3a7a31f062eaf5d246f39e2fd7b09a1bf93e86a7
Habib Ur Rehman <urrehmanhabib443@gmail.com>
2025-05-21 23:39:07 +0500

    JWT Token Verification and Middleware part 1

--- client/src/pages/chat/index.jsx
+++ client/src/pages/chat/index.jsx
@@ -1,4 +1,18 @@
+import { useAppStore } from "@/store";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
 function Chat() {
+  const { userInfo } = useAppStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userInfo.profileSetup) {
+      toast("Please complete your profile first");
+      navigate("/profile");
+    }
+  }, [userInfo, navigate]);
   return <div>Chat</div>;
 }
 
